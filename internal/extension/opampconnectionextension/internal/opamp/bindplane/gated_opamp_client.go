observIQ/bindplane-otel-collector

feat(opamp): Check for `RetryAfterNanoseconds` in error response (#3602)
* WIP: respect RetryAfterNanoseconds in error response

* Add comments to wrapper methods

* Fix unused test param

* Use testing/synctest in gated_opamp_client_test.go

Runs the gate-delay assertions on synctest's fake clock instead of
real sleeps, so they're instant and can assert exact durations
instead of margin-based Less/GreaterOrEqual checks.

Assisted-by: Claude Sonnet 5

* Check for nil, uint64 overflow

* Exclude SetCustomCapabilities from rate limiting

* Close sendGate before collector shutdown

* Make sendGate.wait cancellable

Lets a caller abort a wait early via its own done channel instead of
blocking for the remainder of a server-requested retry delay.

* Let measurementsSender/topologySender cancel a gated send on Stop

Previously Stop() could block for the remainder of a server-requested
retry delay if the sender's loop was parked inside the gated
SendCustomMessage when the server withdrew the capability mid-session,
stalling the OpAMP receiver goroutine that called Stop().

internal/extension/opampconnectionextension/internal/opamp/observiq/gated_opamp_client.go
@@ -1,0 +1,113 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package observiq
+
+import (
+	"context"
+
+	"github.com/open-telemetry/opamp-go/client"
+	"github.com/open-telemetry/opamp-go/protobufs"
+)
+
+// gatedOpAMPClient wraps a client.OpAMPClient so that every call which
+// results in a message being sent to the Server first waits on a sendGate.
+// Start, Stop, and AgentDescription are passed straight through since they
+// don't send new outgoing data (or, in the case of Stop, must never be
+// delayed).
+type gatedOpAMPClient struct {
+	client.OpAMPClient
+	gate *sendGate
+}
+
+// newGatedOpAMPClient wraps underlying so that outgoing messages honor gate.
+func newGatedOpAMPClient(underlying client.OpAMPClient, gate *sendGate) client.OpAMPClient {
+	return &gatedOpAMPClient{OpAMPClient: underlying, gate: gate}
+}
+
+// SetAgentDescription calls [client.OpAMPClient.SetAgentDescription] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetAgentDescription(descr *protobufs.AgentDescription) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetAgentDescription(descr)
+}
+
+// SetHealth calls [client.OpAMPClient.SetHealth] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetHealth(health *protobufs.ComponentHealth) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetHealth(health)
+}
+
+// UpdateEffectiveConfig calls [client.OpAMPClient.UpdateEffectiveConfig] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) UpdateEffectiveConfig(ctx context.Context) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.UpdateEffectiveConfig(ctx)
+}
+
+// SetRemoteConfigStatus calls [client.OpAMPClient.SetRemoteConfigStatus] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetRemoteConfigStatus(status *protobufs.RemoteConfigStatus) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetRemoteConfigStatus(status)
+}
+
+// SetPackageStatuses calls [client.OpAMPClient.SetPackageStatuses] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetPackageStatuses(statuses *protobufs.PackageStatuses) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetPackageStatuses(statuses)
+}
+
+// RequestConnectionSettings calls [client.OpAMPClient.RequestConnectionSettings] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) RequestConnectionSettings(request *protobufs.ConnectionSettingsRequest) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.RequestConnectionSettings(request)
+}
+
+// SetCustomCapabilities calls [client.OpAMPClient.SetCustomCapabilities] with its OpAMPClient.
+// It does not wait for the sendGate
+func (g *gatedOpAMPClient) SetCustomCapabilities(customCapabilities *protobufs.CustomCapabilities) error {
+	return g.OpAMPClient.SetCustomCapabilities(customCapabilities)
+}
+
+// SetFlags calls [client.OpAMPClient.SetFlags] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetFlags(flags protobufs.AgentToServerFlags) {
+	g.gate.wait(nil)
+	g.OpAMPClient.SetFlags(flags)
+}
+
+// SendCustomMessage calls [client.OpAMPClient.SendCustomMessage] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SendCustomMessage(message *protobufs.CustomMessage) (chan struct{}, error) {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SendCustomMessage(message)
+}
+
+// SetAvailableComponents calls [client.OpAMPClient.SetAvailableComponents] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetAvailableComponents(components *protobufs.AvailableComponents) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetAvailableComponents(components)
+}
+
+// SetCapabilities calls [client.OpAMPClient.SetCapabilities] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetCapabilities(capabilities *protobufs.AgentCapabilities) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetCapabilities(capabilities)
+}

internal/extension/opampconnectionextension/internal/opamp/observiq/gated_opamp_client_test.go
@@ -1,0 +1,87 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package observiq
+
+import (
+	"context"
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/observiq/bindplane-otel-collector/internal/extension/opampconnectionextension/internal/opamp/mocks"
+	"github.com/open-telemetry/opamp-go/protobufs"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGatedOpAMPClient_WaitsForGateBeforeDelegating(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		gate := newSendGate()
+		wait := 100 * time.Millisecond
+		gate.block(wait)
+
+		mockClient := new(mocks.MockOpAMPClient)
+		var calledAt time.Time
+		mockClient.On("SetHealth", mock.Anything).Return(nil).Run(func(_ mock.Arguments) {
+			calledAt = time.Now()
+		})
+
+		gated := newGatedOpAMPClient(mockClient, gate)
+
+		start := time.Now()
+		err := gated.SetHealth(&protobufs.ComponentHealth{})
+		assert.NoError(t, err)
+		assert.Equal(t, wait, calledAt.Sub(start))
+
+		mockClient.AssertExpectations(t)
+	})
+}
+
+func TestGatedOpAMPClient_NoDelayWhenGateOpen(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		gate := newSendGate()
+
+		mockClient := new(mocks.MockOpAMPClient)
+		mockClient.On("SendCustomMessage", mock.Anything).Return((chan struct{})(nil), nil)
+
+		gated := newGatedOpAMPClient(mockClient, gate)
+
+		start := time.Now()
+		_, err := gated.SendCustomMessage(&protobufs.CustomMessage{})
+		assert.NoError(t, err)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+
+		mockClient.AssertExpectations(t)
+	})
+}
+
+func TestGatedOpAMPClient_StopIsNotGated(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		gate := newSendGate()
+		gate.block(10 * time.Second)
+
+		mockClient := new(mocks.MockOpAMPClient)
+		mockClient.On("Stop", mock.Anything).Return(nil)
+
+		gated := newGatedOpAMPClient(mockClient, gate)
+
+		start := time.Now()
+		err := gated.Stop(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+
+		mockClient.AssertExpectations(t)
+	})
+}

internal/extension/opampconnectionextension/internal/opamp/observiq/send_gate.go
@@ -1,0 +1,87 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package observiq
+
+import (
+	"sync"
+	"time"
+)
+
+// sendGate blocks outgoing OpAMP messages until a server-requested retry
+// delay (see ServerErrorResponse.RetryInfo) has elapsed. It is shared between
+// the Client and its gatedOpAMPClient so that a delay observed in
+// onErrorHandler is enforced on every subsequent send attempt, rather than
+// just delaying receipt of the next server message.
+type sendGate struct {
+	mu        sync.Mutex
+	notBefore time.Time
+
+	closeOnce sync.Once
+	closed    chan struct{}
+}
+
+func newSendGate() *sendGate {
+	return &sendGate{closed: make(chan struct{})}
+}
+
+// block delays outgoing messages until d has elapsed. If a longer delay is
+// already in effect, the longer one wins.
+func (g *sendGate) block(d time.Duration) {
+	if d <= 0 {
+		return
+	}
+
+	notBefore := time.Now().Add(d)
+
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	if notBefore.After(g.notBefore) {
+		g.notBefore = notBefore
+	}
+}
+
+// wait blocks until any delay set by block has elapsed, or the gate is closed.
+// If cancel fires first, wait returns false to indicate that the caller should abandon whatever send it was waiting to make;
+// a nil cancel behaves as if it never fires. A nil *sendGate always returns true immediately,
+// so callers that only get a gate when it's needed (e.g. in tests) don't need to guard against it.
+func (g *sendGate) wait(cancel <-chan struct{}) bool {
+	if g == nil {
+		return true
+	}
+
+	for {
+		g.mu.Lock()
+		remaining := time.Until(g.notBefore)
+		g.mu.Unlock()
+
+		if remaining <= 0 {
+			return true
+		}
+
+		select {
+		case <-time.After(remaining):
+		case <-g.closed:
+			return true
+		case <-cancel:
+			return false
+		}
+	}
+}
+
+// close releases any goroutines currently blocked in wait. Safe to call
+// multiple times.
+func (g *sendGate) close() {
+	g.closeOnce.Do(func() { close(g.closed) })
+}

internal/extension/opampconnectionextension/internal/opamp/observiq/send_gate_test.go
@@ -1,0 +1,145 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package observiq
+
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSendGate_WaitWithoutBlockReturnsImmediately(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+	})
+}
+
+func TestSendGate_BlockDelaysWait(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		wait := 100 * time.Millisecond
+		g.block(wait)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, wait, time.Since(start))
+	})
+}
+
+func TestSendGate_LongerBlockWins(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		g.block(50 * time.Millisecond)
+		g.block(200 * time.Millisecond)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, 200*time.Millisecond, time.Since(start))
+	})
+}
+
+func TestSendGate_ShorterBlockDoesNotShortenExistingDelay(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		g.block(200 * time.Millisecond)
+		g.block(50 * time.Millisecond)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, 200*time.Millisecond, time.Since(start))
+	})
+}
+
+func TestSendGate_CloseUnblocksWait(t *testing.T) {
+	synctest.Test(t, func(_ *testing.T) {
+		g := newSendGate()
+		g.block(10 * time.Second)
+
+		done := make(chan struct{})
+		go func() {
+			g.wait(nil)
+			close(done)
+		}()
+
+		g.close()
+		<-done
+	})
+}
+
+func TestSendGate_CancelUnblocksWaitAndReturnsFalse(t *testing.T) {
+	synctest.Test(t, func(_ *testing.T) {
+		g := newSendGate()
+		g.block(10 * time.Second)
+
+		cancel := make(chan struct{})
+		result := make(chan bool, 1)
+		go func() {
+			result <- g.wait(cancel)
+		}()
+
+		close(cancel)
+		assert.False(t, <-result)
+	})
+}
+
+func TestSendGate_WaitReturnsTrueWhenDelayElapsesBeforeCancel(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+		g.block(50 * time.Millisecond)
+
+		assert.True(t, g.wait(make(chan struct{})))
+	})
+}
+
+func TestSendGate_NilGateWaitReturnsTrueImmediately(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		var g *sendGate
+
+		start := time.Now()
+		assert.True(t, g.wait(nil))
+		assert.Equal(t, time.Duration(0), time.Since(start))
+	})
+}
+
+func TestSendGate_CloseIsIdempotent(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+		assert.NotPanics(t, func() {
+			g.close()
+			g.close()
+		})
+	})
+}
+
+func TestSendGate_NonPositiveBlockIsNoop(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+		g.block(0)
+		g.block(-1 * time.Second)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+	})
+}

Dynatrace/dynatrace-bindplane-otel-collector

internal/extension/opampconnectionextension/internal/opamp/bindplane/gated_opamp_client.go
@@ -1,0 +1,113 @@
+// Copyright Dynatrace LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bindplane
+
+import (
+	"context"
+
+	"github.com/open-telemetry/opamp-go/client"
+	"github.com/open-telemetry/opamp-go/protobufs"
+)
+
+// gatedOpAMPClient wraps a client.OpAMPClient so that every call which
+// results in a message being sent to the Server first waits on a sendGate.
+// Start, Stop, and AgentDescription are passed straight through since they
+// don't send new outgoing data (or, in the case of Stop, must never be
+// delayed).
+type gatedOpAMPClient struct {
+	client.OpAMPClient
+	gate *sendGate
+}
+
+// newGatedOpAMPClient wraps underlying so that outgoing messages honor gate.
+func newGatedOpAMPClient(underlying client.OpAMPClient, gate *sendGate) client.OpAMPClient {
+	return &gatedOpAMPClient{OpAMPClient: underlying, gate: gate}
+}
+
+// SetAgentDescription calls [client.OpAMPClient.SetAgentDescription] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetAgentDescription(descr *protobufs.AgentDescription) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetAgentDescription(descr)
+}
+
+// SetHealth calls [client.OpAMPClient.SetHealth] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetHealth(health *protobufs.ComponentHealth) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetHealth(health)
+}
+
+// UpdateEffectiveConfig calls [client.OpAMPClient.UpdateEffectiveConfig] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) UpdateEffectiveConfig(ctx context.Context) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.UpdateEffectiveConfig(ctx)
+}
+
+// SetRemoteConfigStatus calls [client.OpAMPClient.SetRemoteConfigStatus] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetRemoteConfigStatus(status *protobufs.RemoteConfigStatus) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetRemoteConfigStatus(status)
+}
+
+// SetPackageStatuses calls [client.OpAMPClient.SetPackageStatuses] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetPackageStatuses(statuses *protobufs.PackageStatuses) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetPackageStatuses(statuses)
+}
+
+// RequestConnectionSettings calls [client.OpAMPClient.RequestConnectionSettings] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) RequestConnectionSettings(request *protobufs.ConnectionSettingsRequest) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.RequestConnectionSettings(request)
+}
+
+// SetCustomCapabilities calls [client.OpAMPClient.SetCustomCapabilities] with its OpAMPClient.
+// It does not wait for the sendGate
+func (g *gatedOpAMPClient) SetCustomCapabilities(customCapabilities *protobufs.CustomCapabilities) error {
+	return g.OpAMPClient.SetCustomCapabilities(customCapabilities)
+}
+
+// SetFlags calls [client.OpAMPClient.SetFlags] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetFlags(flags protobufs.AgentToServerFlags) {
+	g.gate.wait(nil)
+	g.OpAMPClient.SetFlags(flags)
+}
+
+// SendCustomMessage calls [client.OpAMPClient.SendCustomMessage] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SendCustomMessage(message *protobufs.CustomMessage) (chan struct{}, error) {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SendCustomMessage(message)
+}
+
+// SetAvailableComponents calls [client.OpAMPClient.SetAvailableComponents] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetAvailableComponents(components *protobufs.AvailableComponents) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetAvailableComponents(components)
+}
+
+// SetCapabilities calls [client.OpAMPClient.SetCapabilities] with its OpAMPClient after making sure
+// the sendGate isn't blocking
+func (g *gatedOpAMPClient) SetCapabilities(capabilities *protobufs.AgentCapabilities) error {
+	g.gate.wait(nil)
+	return g.OpAMPClient.SetCapabilities(capabilities)
+}

internal/extension/opampconnectionextension/internal/opamp/bindplane/gated_opamp_client_test.go
@@ -1,0 +1,87 @@
+// Copyright Dynatrace LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bindplane
+
+import (
+	"context"
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/dynatrace/dynatrace-bindplane-otel-collector/internal/extension/opampconnectionextension/internal/opamp/mocks"
+	"github.com/open-telemetry/opamp-go/protobufs"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGatedOpAMPClient_WaitsForGateBeforeDelegating(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		gate := newSendGate()
+		wait := 100 * time.Millisecond
+		gate.block(wait)
+
+		mockClient := new(mocks.MockOpAMPClient)
+		var calledAt time.Time
+		mockClient.On("SetHealth", mock.Anything).Return(nil).Run(func(_ mock.Arguments) {
+			calledAt = time.Now()
+		})
+
+		gated := newGatedOpAMPClient(mockClient, gate)
+
+		start := time.Now()
+		err := gated.SetHealth(&protobufs.ComponentHealth{})
+		assert.NoError(t, err)
+		assert.Equal(t, wait, calledAt.Sub(start))
+
+		mockClient.AssertExpectations(t)
+	})
+}
+
+func TestGatedOpAMPClient_NoDelayWhenGateOpen(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		gate := newSendGate()
+
+		mockClient := new(mocks.MockOpAMPClient)
+		mockClient.On("SendCustomMessage", mock.Anything).Return((chan struct{})(nil), nil)
+
+		gated := newGatedOpAMPClient(mockClient, gate)
+
+		start := time.Now()
+		_, err := gated.SendCustomMessage(&protobufs.CustomMessage{})
+		assert.NoError(t, err)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+
+		mockClient.AssertExpectations(t)
+	})
+}
+
+func TestGatedOpAMPClient_StopIsNotGated(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		gate := newSendGate()
+		gate.block(10 * time.Second)
+
+		mockClient := new(mocks.MockOpAMPClient)
+		mockClient.On("Stop", mock.Anything).Return(nil)
+
+		gated := newGatedOpAMPClient(mockClient, gate)
+
+		start := time.Now()
+		err := gated.Stop(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+
+		mockClient.AssertExpectations(t)
+	})
+}

internal/extension/opampconnectionextension/internal/opamp/bindplane/send_gate.go
@@ -1,0 +1,87 @@
+// Copyright Dynatrace LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bindplane
+
+import (
+	"sync"
+	"time"
+)
+
+// sendGate blocks outgoing OpAMP messages until a server-requested retry
+// delay (see ServerErrorResponse.RetryInfo) has elapsed. It is shared between
+// the Client and its gatedOpAMPClient so that a delay observed in
+// onErrorHandler is enforced on every subsequent send attempt, rather than
+// just delaying receipt of the next server message.
+type sendGate struct {
+	mu        sync.Mutex
+	notBefore time.Time
+
+	closeOnce sync.Once
+	closed    chan struct{}
+}
+
+func newSendGate() *sendGate {
+	return &sendGate{closed: make(chan struct{})}
+}
+
+// block delays outgoing messages until d has elapsed. If a longer delay is
+// already in effect, the longer one wins.
+func (g *sendGate) block(d time.Duration) {
+	if d <= 0 {
+		return
+	}
+
+	notBefore := time.Now().Add(d)
+
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	if notBefore.After(g.notBefore) {
+		g.notBefore = notBefore
+	}
+}
+
+// wait blocks until any delay set by block has elapsed, or the gate is closed.
+// If cancel fires first, wait returns false to indicate that the caller should abandon whatever send it was waiting to make;
+// a nil cancel behaves as if it never fires. A nil *sendGate always returns true immediately,
+// so callers that only get a gate when it's needed (e.g. in tests) don't need to guard against it.
+func (g *sendGate) wait(cancel <-chan struct{}) bool {
+	if g == nil {
+		return true
+	}
+
+	for {
+		g.mu.Lock()
+		remaining := time.Until(g.notBefore)
+		g.mu.Unlock()
+
+		if remaining <= 0 {
+			return true
+		}
+
+		select {
+		case <-time.After(remaining):
+		case <-g.closed:
+			return true
+		case <-cancel:
+			return false
+		}
+	}
+}
+
+// close releases any goroutines currently blocked in wait. Safe to call
+// multiple times.
+func (g *sendGate) close() {
+	g.closeOnce.Do(func() { close(g.closed) })
+}

internal/extension/opampconnectionextension/internal/opamp/bindplane/send_gate_test.go
@@ -1,0 +1,145 @@
+// Copyright Dynatrace LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bindplane
+
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSendGate_WaitWithoutBlockReturnsImmediately(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+	})
+}
+
+func TestSendGate_BlockDelaysWait(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		wait := 100 * time.Millisecond
+		g.block(wait)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, wait, time.Since(start))
+	})
+}
+
+func TestSendGate_LongerBlockWins(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		g.block(50 * time.Millisecond)
+		g.block(200 * time.Millisecond)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, 200*time.Millisecond, time.Since(start))
+	})
+}
+
+func TestSendGate_ShorterBlockDoesNotShortenExistingDelay(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+
+		g.block(200 * time.Millisecond)
+		g.block(50 * time.Millisecond)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, 200*time.Millisecond, time.Since(start))
+	})
+}
+
+func TestSendGate_CloseUnblocksWait(t *testing.T) {
+	synctest.Test(t, func(_ *testing.T) {
+		g := newSendGate()
+		g.block(10 * time.Second)
+
+		done := make(chan struct{})
+		go func() {
+			g.wait(nil)
+			close(done)
+		}()
+
+		g.close()
+		<-done
+	})
+}
+
+func TestSendGate_CancelUnblocksWaitAndReturnsFalse(t *testing.T) {
+	synctest.Test(t, func(_ *testing.T) {
+		g := newSendGate()
+		g.block(10 * time.Second)
+
+		cancel := make(chan struct{})
+		result := make(chan bool, 1)
+		go func() {
+			result <- g.wait(cancel)
+		}()
+
+		close(cancel)
+		assert.False(t, <-result)
+	})
+}
+
+func TestSendGate_WaitReturnsTrueWhenDelayElapsesBeforeCancel(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+		g.block(50 * time.Millisecond)
+
+		assert.True(t, g.wait(make(chan struct{})))
+	})
+}
+
+func TestSendGate_NilGateWaitReturnsTrueImmediately(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		var g *sendGate
+
+		start := time.Now()
+		assert.True(t, g.wait(nil))
+		assert.Equal(t, time.Duration(0), time.Since(start))
+	})
+}
+
+func TestSendGate_CloseIsIdempotent(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+		assert.NotPanics(t, func() {
+			g.close()
+			g.close()
+		})
+	})
+}
+
+func TestSendGate_NonPositiveBlockIsNoop(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		g := newSendGate()
+		g.block(0)
+		g.block(-1 * time.Second)
+
+		start := time.Now()
+		g.wait(nil)
+		assert.Equal(t, time.Duration(0), time.Since(start))
+	})
+}